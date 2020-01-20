22aea175754edd12b08243e918779be38ec0245c
ht Tan <45584776+CyrilKZ@users.noreply.github.com>
2020-01-20 16:36:46 +0800

    fixed vuetify

--- template/src/main.js
+++ template/src/main.js
@@ -1,5 +1,3 @@
-import vuetify from "./vuetify/vuetify"
-
 {{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
